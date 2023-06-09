4cd4de555a9bfbbaac3889da8db891432985e975
Iain <iain.mccall.95@gmail.com>
2023-06-09 15:06:06 +0100

    started file structure and tested db

---
+++ db/shop_inventory.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS merchants;
+
+CREATE TABLE merchants (
+    id SERIAL PRIMARY KEY,
+    merchant_name VARCHAR(255),
+    alignment VARCHAR(255)
+);
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    product_name VARCHAR(255),
+    product_description VARCHAR (255),
+    stock_quantity INT,
+    buying_cost INT,
+    selling_cost INT,
+    category VARCHAR(255),
+    merchant_id INT NOT NULL REFERENCES merchants(id),
+    min_stock_level INT
+);
+
+INSERT INTO merchants (merchant_name, alignment) VALUES ('ganglebert', 'lawful good');
+INSERT INTO products (product_name, product_description, stock_quantity, buying_cost, selling_cost, category, merchant_id, min_stock_level) VALUES
+('Hogweed', 'Smells bad', '12', '1', '15', 'ingredient', '1','15');
